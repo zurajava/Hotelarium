5f120d3967f789161420d32d1cf1057f3dd1603b
zura <zurikacitadze@gmail.com>
2017-11-07 14:21:49 +0400

    r

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -7,9 +7,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { GridModule } from '@progress/kendo-angular-grid';
 import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
+import { ChartsModule } from 'ng2-charts';
 
 @NgModule({
-  imports: [DashboardRoutingModule, SharedModule, BrowserModule, BrowserAnimationsModule, GridModule, DropDownsModule],
+  imports: [DashboardRoutingModule, SharedModule, BrowserModule, BrowserAnimationsModule, GridModule, DropDownsModule, ChartsModule],
   declarations: [DashboardComponent],
   providers: [DashboardService]
 })
