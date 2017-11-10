13101b1312900f182dee591ff9250ea2e80c5391
zura <zurikacitadze@gmail.com>
2017-11-10 17:23:02 +0400

    vvv

--- src/app/reservation/reservation.component.ts
+++ src/app/reservation/reservation.component.ts
@@ -104,6 +104,13 @@ export class ReservationComponent implements OnInit {
       reservation.room = data.json().room;
     });
   }
+  public serviceValueChange(newValue, service: ReservationServices): void {
+    for (var i = 0; i < this.serviceData.length; i++) {
+      if (newValue == this.serviceData[i].id) {
+        service.frequency = this.serviceData[i].type;
+      }
+    }
+  }
   fillDataRange() {
     const currentDate = this.dateFrom;
     const datesArray = [];
