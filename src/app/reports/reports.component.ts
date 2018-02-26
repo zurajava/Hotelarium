b2ce0b71e77c61c273a48320b36289f12fb2aaa8
zura <zurikacitadze@gmail.com>
2018-02-26 18:20:01 +0400

    as

--- src/app/reports/reports.component.ts
+++ src/app/reports/reports.component.ts
@@ -8,6 +8,7 @@ import * as moment from 'moment';
 import { NgbModal, ModalDismissReasons, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { PaymentReport, PaymentOverall, PaymentDetailed } from './model';
 import { IntlService } from '@progress/kendo-angular-intl';
+import { Subscription } from 'rxjs/Subscription';
 declare var $: any;
 
 @Component({
@@ -18,6 +19,7 @@ declare var $: any;
 })
 
 export class ReportsComponent implements OnInit {
+  subscription: Subscription;
   public userBranch: Array<any>;
 
   public dateFrom: Date;
@@ -26,11 +28,9 @@ export class ReportsComponent implements OnInit {
 
   public dateFromDetailed: Date;
   public dateToDetailed: Date;
-  public brSelectedValueDetailed: number;
 
   public dateFromOverall: Date;
   public dateToOverall: Date;
-  public brSelectedValueOverall: number;
 
   public data: Array<PaymentReport>;
   public paymentDetailed: Array<PaymentDetailed>;
@@ -56,90 +56,68 @@ export class ReportsComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.reportsService.getUserBranch(this.authservice.getUserID()).subscribe(data => {
-      if (data.json().success === true) {
-        this.userBranch = data.json().branch;
-        this.brSelectedValue = this.userBranch[0].id;
-        this.brSelectedValueDetailed = this.userBranch[0].id;
-        this.brSelectedValueOverall = this.userBranch[0].id;
-
-        this.reportsService.getPaymentReport(this.brSelectedValue.toString(), this.intl.formatDate(this.dateFrom, 'yyyy-MM-dd'),
-          this.intl.formatDate(this.dateTo, 'yyyy-MM-dd')).then(data => {
-            if (data.success === true) {
-              this.data = data.payments;
-              console.log("this.data", this.data);
-              $(function () {
-                $("#example1").DataTable({
-                  'paging': false,
-                  'lengthChange': false,
-                  'searching': false,
-                  'ordering': false,
-                  'info': false,
-                  'autoWidth': false
-                });
+    this.subscription = this.authservice.getMessage().subscribe(message => {
+      this.brSelectedValue = message;
+      this.reportsService.getPaymentReport(this.brSelectedValue.toString(), this.intl.formatDate(this.dateFrom, 'yyyy-MM-dd'),
+        this.intl.formatDate(this.dateTo, 'yyyy-MM-dd')).then(data => {
+          if (data.success === true) {
+            this.data = data.payments;
+            console.log("this.data", this.data);
+            $(function () {
+              $("#example1").DataTable({
+                'paging': false,
+                'lengthChange': false,
+                'searching': false,
+                'ordering': false,
+                'info': false,
+                'autoWidth': false
               });
-            } else {
-              this.toastr.error(data.json().message);
-            }
-          });
-        this.reportsService.getPaymentOverall(this.brSelectedValueOverall.toString(), this.intl.formatDate(this.dateFromOverall, 'yyyy-MM-dd'),
-          this.intl.formatDate(this.dateToOverall, 'yyyy-MM-dd')).then(data => {
-            if (data.success === true) {
-              this.paymentOverall = data.data;
-              console.log("this.paymentOverall", this.paymentOverall);
-              $(function () {
-                $("#example2").DataTable({
-                  'paging': false,
-                  'lengthChange': false,
-                  'searching': false,
-                  'ordering': false,
-                  'info': false,
-                  'autoWidth': false
-                });
+            });
+          } else {
+            this.toastr.error(data.json().message);
+          }
+        });
+      this.reportsService.getPaymentOverall(this.brSelectedValue.toString(), this.intl.formatDate(this.dateFromOverall, 'yyyy-MM-dd'),
+        this.intl.formatDate(this.dateToOverall, 'yyyy-MM-dd')).then(data => {
+          if (data.success === true) {
+            this.paymentOverall = data.data;
+            console.log("this.paymentOverall", this.paymentOverall);
+            $(function () {
+              $("#example2").DataTable({
+                'paging': false,
+                'lengthChange': false,
+                'searching': false,
+                'ordering': false,
+                'info': false,
+                'autoWidth': false
               });
-            } else {
-              this.toastr.error(data.json().message);
-            }
-          });
-        this.reportsService.getPaymentDetailed(this.brSelectedValueDetailed.toString(), this.intl.formatDate(this.dateFromDetailed, 'yyyy-MM-dd'),
-          this.intl.formatDate(this.dateToDetailed, 'yyyy-MM-dd')).then(data => {
-            if (data.success === true) {
-              this.paymentDetailed = data.data;
-              console.log("this.paymentDetailed", this.paymentDetailed);
-              $(function () {
-                $("#example3").DataTable({
-                  'paging': false,
-                  'lengthChange': false,
-                  'searching': false,
-                  'ordering': false,
-                  'info': false,
-                  'autoWidth': false
-                });
+            });
+          } else {
+            this.toastr.error(data.json().message);
+          }
+        });
+      this.reportsService.getPaymentDetailed(this.brSelectedValue.toString(), this.intl.formatDate(this.dateFromDetailed, 'yyyy-MM-dd'),
+        this.intl.formatDate(this.dateToDetailed, 'yyyy-MM-dd')).then(data => {
+          if (data.success === true) {
+            this.paymentDetailed = data.data;
+            console.log("this.paymentDetailed", this.paymentDetailed);
+            $(function () {
+              $("#example3").DataTable({
+                'paging': false,
+                'lengthChange': false,
+                'searching': false,
+                'ordering': false,
+                'info': false,
+                'autoWidth': false
               });
-            } else {
-              this.toastr.error(data.json().message);
-            }
-          });
-
-
-      } else {
-        this.toastr.error(data.json().message);
-      }
+            });
+          } else {
+            this.toastr.error(data.json().message);
+          }
+        });
     });
-  }
-  public brValueChange(value: any): void {
-    this.brSelectedValue = value;
-    this.loadRezervationPayment();
-  }
-  public brValueChangeOverall(value: any): void {
-    this.brSelectedValueOverall = value;
-    this.loadRezervationPaymentOwerall();
-  }
-  public brValueChangeDetailed(value: any): void {
-    this.brSelectedValueDetailed = value;
-    this.loadRezervationPaymentDetailed();
-  }
 
+  }
   filterPayment() {
     this.loadRezervationPayment();
   }
@@ -163,7 +141,7 @@ export class ReportsComponent implements OnInit {
   }
   loadRezervationPaymentDetailed() {
     this.paymentDetailed = null;
-    this.reportsService.getPaymentDetailed(this.brSelectedValueDetailed.toString(), this.intl.formatDate(this.dateFromDetailed, 'yyyy-MM-dd'),
+    this.reportsService.getPaymentDetailed(this.brSelectedValue.toString(), this.intl.formatDate(this.dateFromDetailed, 'yyyy-MM-dd'),
       this.intl.formatDate(this.dateToDetailed, 'yyyy-MM-dd')).then(data => {
         if (data.success === true) {
           this.paymentDetailed = data.data;
@@ -174,7 +152,7 @@ export class ReportsComponent implements OnInit {
   }
   loadRezervationPaymentOwerall() {
     this.paymentOverall = null;
-    this.reportsService.getPaymentOverall(this.brSelectedValueOverall.toString(), this.intl.formatDate(this.dateFromOverall, 'yyyy-MM-dd'),
+    this.reportsService.getPaymentOverall(this.brSelectedValue.toString(), this.intl.formatDate(this.dateFromOverall, 'yyyy-MM-dd'),
       this.intl.formatDate(this.dateToOverall, 'yyyy-MM-dd')).then(data => {
         if (data.success === true) {
           this.paymentOverall = data.data;
